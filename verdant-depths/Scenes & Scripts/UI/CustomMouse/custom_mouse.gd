e187c4a600420bf723128fe9d7bba4b6b6582a58
Nicholas Kuün <nickuun@gmail.com>
2025-06-02 19:17:12 +0200

    Added Custom Mouse

---
+++ verdant-depths/Scenes & Scripts/UI/CustomMouse/custom_mouse.gd
@@ -0,0 +1,21 @@
+extends Control
+class_name CustomMouse
+
+@onready var cursor_sprite: AnimatedSprite2D = $CursorSprite
+
+func _physics_process(delta: float) -> void:
+	global_position = get_global_mouse_position()
+
+	# React to input
+	if GameInputEvents.use_tool():
+		play_click_animation()
+	else:
+		play_idle_animation()
+
+func play_click_animation():
+	if not cursor_sprite.is_playing() or cursor_sprite.animation != "click":
+		cursor_sprite.play("click")
+
+func play_idle_animation():
+	if not cursor_sprite.is_playing() or cursor_sprite.animation != "idle":
+		cursor_sprite.play("idle")
