1f7d78be9840720b426c7b218eee167cad27d4db
Nick-AutumnLeaf <70589769+Nick-AutumnLeaf@users.noreply.github.com>
2025-06-14 18:02:15 +0200

    Keychain Start

--- verdant-depths/Scenes & Scripts/UI/CustomMouse/custom_mouse.gd
+++ verdant-depths/Scenes & Scripts/UI/CustomMouse/custom_mouse.gd
@@ -1,10 +1,17 @@
 extends Control
 class_name CustomMouse
 
-@onready var cursor_sprite: AnimatedSprite2D = $CursorSprite
+@onready var cursor_sprite: AnimatedSprite2D = $CharacterBody2D/CursorSprite
+@export var JointB :  Node2D
+
+func _ready() -> void:
+	if JointB:
+		$PinJoint2D.node_b = JointB.get_path()
+		JointB.global_position = get_global_mouse_position()
 
 func _physics_process(delta: float) -> void:
 	global_position = get_global_mouse_position()
+	$PinJoint2D.global_position = get_global_mouse_position()
 
 	# React to input
 	if GameInputEvents.use_tool():
